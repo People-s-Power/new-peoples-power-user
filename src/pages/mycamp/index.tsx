373e11c22c99665f9f3385e012db484d9cfd3598
alabo-excel <iamalaboexcel@gmail.com>
2023-02-02 04:58:37 -1100

    user dashboard fix

--- src/pages/mycamp/index.tsx
+++ src/pages/mycamp/index.tsx
@@ -13,9 +13,13 @@ import { NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { useRecoilValue } from "recoil";
+import { ICampaign } from "types/Applicant.types";
+import { apollo } from "apollo";
+import { MY_EVENT } from "apollo/queries/eventQuery";
+import { GET_USER_POSTS } from 'apollo/queries/postQuery'
+import { MY_ADVERTS } from "apollo/queries/advertsQuery";
 import router, { useRouter } from "next/router";
 import axios from 'axios';
-import { GET_ALL, GET_ALL_USERS, FOLLOW } from "apollo/queries/generalQuery";
 
 import { SERVER_URL } from "utils/constants";
 import { print } from 'graphql';
@@ -23,52 +27,80 @@ dayjs.extend(relativeTime);
 
 const MyCamp: NextPage = (): JSX.Element => {
 	const author = useRecoilValue(UserAtom);
-	const [all, setAll] = useState<any>([])
+	const [petition, setPetition] = useState([])
+	const [post, setPost] = useState([])
+	const [events, setEvents] = useState([])
+	const [adverts, setAdverts] = useState([]);
 	const { query } = useRouter();
-	const [loading, setLoading] = useState(true)
-	// const [campaigns, setCampaigns] = useState([]);
+	const [campaigns, setCampaigns] = useState([]);
 
-	useEffect(() => {
-		console.log(author)
-		async function getData() {
-			try {
-				const { data } = await axios.post(SERVER_URL + '/graphql', {
-					query: print(GET_ALL),
-					variables: {
-						authorId: author.id
-					}
-				})
-				console.log(data.data.timeline)
-				let general = [...data.data.timeline.adverts, {
-					"__typename": 'Follow'
-				}, ...data.data.timeline.updates, {
-					"__typename": 'Follow'
-				}, ...data.data.timeline.events, {
-					"__typename": 'Follow'
-				}, ...data.data.timeline.petitions, {
-					"__typename": 'Follow'
-				}, ...data.data.timeline.posts, {
-					"__typename": 'Follow'
-				}, ...data.data.timeline.victories, {
-					"__typename": 'Follow'
-				}]
-				const randomize = (values: any) => {
-					let index = values.length, randomIndex;
-					while (index != 0) {
-						randomIndex = Math.floor(Math.random() * index);
-						index--;
-						[values[index], values[randomIndex]] = [
-							values[randomIndex], values[index]];
-					}
-					return values;
-				}
-				randomize(general)
-				setAll(general)
-				setLoading(false)
-			} catch (err) {
-				console.log(err)
+	// const loading = true;
+	const getGeneral = () => {
+		let general = [...petition, ...post, ...adverts, ...events]
+		const randomize = (values: any) => {
+			let index = values.length, randomIndex;
+			while (index != 0) {
+				randomIndex = Math.floor(Math.random() * index);
+				index--;
+				[values[index], values[randomIndex]] = [
+					values[randomIndex], values[index]];
 			}
+			return values;
 		}
+		randomize(general)
+		setCampaigns(general)
+		// console.log(all)
+	}
+
+	useQuery(MY_PETITION, {
+		client: apollo,
+		onCompleted: (data) => {
+			setPetition(data.myPetition)
+			console.log(data)
+			getGeneral()
+		},
+		onError: (e) => console.log(e),
+	});
+	useQuery(MY_ADVERTS, {
+		client: apollo,
+		variables: { authorId: author?.id },
+		onCompleted: (data) => {
+			console.log(data)
+			setAdverts(data.myAdverts)
+			getGeneral()
+		},
+		onError: (err) => {
+		},
+	});
+	useQuery(GET_USER_POSTS, {
+		client: apollo,
+		onCompleted: (data) => {
+			// console.log(data)
+			setPost(data.myPosts)
+			getGeneral()
+		},
+		onError: (err) => {
+		},
+	});
+
+	const getEvent = async () => {
+		try {
+			const { data } = await axios.post(SERVER_URL + '/graphql', {
+				query: print(MY_EVENT),
+				variables: {
+					authorId: author?.id,
+					page: 1
+				}
+			})
+			console.log(data)
+			setEvents(data.myEvents)
+		} catch (error) {
+			console.log(error);
+		}
+	}
+	useEffect(() => {
+		getEvent()
+		getGeneral()
 	}, [])
 	return (
 		<FrontLayout showFooter={false}>
@@ -88,26 +120,18 @@ const MyCamp: NextPage = (): JSX.Element => {
 							</a>
 						</Link> */}
 						<div className="mt-4 ">
-							{loading ? (
-								<p>Loading...</p>
-							) : all?.length ? (
-								<div>
-									{/* <div className="slide-sec mb-3">
-										<Slider />
-									</div> */}
-								</div>
-							) : (
-								<p className="text-center">Start by creating a new campaign</p>
-							)}
-							{all?.length > 0 && (
+
+							{campaigns.length > 0 ? (
 								<div>
 									<h3 className="fs-4 fw-bold text-center">Check Campaign Progress</h3>
 									<div className="d-flex py-3 flex-column flex-md-row">
 										<div className="flex-fill overflow-auto">
-											{/* <CampaignTable campaigns={all} /> */}
+											<CampaignTable campaigns={campaigns} />
 										</div>
 									</div>
 								</div>
+							) : (
+								<p>Loading...</p>
 							)}
 						</div>
 					</div>
