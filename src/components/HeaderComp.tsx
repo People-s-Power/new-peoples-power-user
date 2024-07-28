333e1a0fce5dacb49faf373404c9e5d8c8c422be
SpiffGreen <spiffjekeygreen@gmail.com>
2024-07-28 17:44:05 +0100

    feat: add auto login with token

--- src/components/HeaderComp.tsx
+++ src/components/HeaderComp.tsx
@@ -1,67 +1,85 @@
-import { UserAtom } from "atoms/UserAtom";
-import Link from "next/link";
-import { useRouter } from "next/router";
-import React, { useState, useEffect } from "react";
-import { useRecoilValue } from "recoil";
-import UserMenu from "./user-profile/UserMenu";
-import { Dropdown } from 'rsuite';
+import { UserAtom } from "atoms/UserAtom"
+import Link from "next/link"
+import { useRouter } from "next/router"
+import React, { useState, useEffect } from "react"
+import { useRecoilValue } from "recoil"
+import UserMenu from "./user-profile/UserMenu"
+import { Dropdown } from "rsuite"
 // import { SERVER_URL } from "utils/constants"
 // import { io } from "socket.io-client"
 import { socket } from "pages/_app"
 import { TOKEN_NAME } from "utils/constants"
 import cookie from "js-cookie"
+import axios from "axios"
+import { toast } from "react-toastify"
 
 const Header = (): JSX.Element => {
-	const user = useRecoilValue(UserAtom);
-	const [menu, setMenu] = useState(false);
-	const [count, setCount] = useState(0);
+	const user = useRecoilValue(UserAtom)
+	const [menu, setMenu] = useState(false)
+	const [count, setCount] = useState(0)
 	const [messageCount, setMessageCount] = useState(0)
-	// const socket = io(SERVER_URL, {
-	// 	query: {
-	// 		user_id: user?.id,
-	// 	},
-	// })
 	const token = cookie.get(TOKEN_NAME)
-
-	// useEffect(() => {
-	// 	const route = window.location.pathname
-	// 	// console.log(route)
-	// 	if (route === '/about' || '/campaigns' || '/events' || '/contact') {
-	// 		return
-	// 	}
-	// }, [])
 
 	useEffect(() => {
 		const route = window.location.pathname
 
 		if (token === undefined && user === null) {
-			if (route === "/about" || route === "/campaigns" || route === "/events" || route === "/contact" || route === "/home" || route === "/docs/CreatingEventDoc") {
+			if (
+				route === "/about" ||
+				route === "/campaigns" ||
+				route === "/events" ||
+				route === "/contact" ||
+				route === "/home" ||
+				route === "/docs/CreatingEventDoc"
+			) {
 				console.log(true)
 			} else {
-				window.location.href = "/home"
+				const searchParams = new URLSearchParams(window.location.search)
+				if (searchParams.has("tid")) {
+					const accessToken = searchParams.get("tid")
+					// verify and login user
+					axios
+						.post("/auth/login-with-token", { accessToken })
+						.then(({ data }) => {
+							cookie.set("user_id", data?.id)
+							cookie.set(TOKEN_NAME, data?.token)
+							localStorage.setItem("page", user?.id);
+							window.location.href = window.location.pathname;
+						})
+						.catch((error) => {
+							const e = error as any
+							console.log({ error })
+							toast(e?.response?.data?.message)
+						})
+				} else {
+					window.location.href = "/home"
+				}
 			}
 		}
 	}, [])
 
 	useEffect(() => {
 		if (socket.connected) {
-			socket.emit("notifications", {
-				userId: user?.id,
-				page: 1,
-				limit: 80
-			}, (response) => {
-				// console.log(response)
-				setCount(response.unReadCount)
-			})
+			socket.emit(
+				"notifications",
+				{
+					userId: user?.id,
+					page: 1,
+					limit: 80,
+				},
+				(response) => {
+					// console.log(response)
+					setCount(response.unReadCount)
+				}
+			)
 		}
 	}, [user])
 
 	useEffect(() => {
-
-		socket.emit('unread_count', user?.id, response =>
+		socket.emit("unread_count", user?.id, (response) =>
 			// console.log('unread_count:', response),
 			setMessageCount(response)
-		);
+		)
 
 		// socket.emit('unread_count', {
 		// 	user_id: user?.id
@@ -71,12 +89,13 @@ const Header = (): JSX.Element => {
 		// });
 	}, [user])
 
-	const { pathname } = useRouter();
+	const { pathname } = useRouter()
 	const text = `EXPERTHUB`
 	// const token = cookie.get(TOKEN_NAME);
 
 	// eslint-disable-next-line no-empty
-	if (typeof window !== 'undefined') { }
+	if (typeof window !== "undefined") {
+	}
 
 	const navItems = (loggedIn: boolean) => [
 		{ title: "Home", link: loggedIn ? `/feeds` : "auth" },
@@ -84,7 +103,7 @@ const Header = (): JSX.Element => {
 		// { title: "Messages", link: "/messages" },
 		{ title: "My Connection", link: "/connection" },
 		// { title: "Explore", link: "campaigns" },
-	];
+	]
 
 	return (
 		<header>
@@ -98,24 +117,19 @@ const Header = (): JSX.Element => {
 						</a>
 						{/* </Link> */}
 						<div className="flex d-md-none">
-							{
-								user &&
-								<Link href={'/messages'}>
+							{user && (
+								<Link href={"/messages"}>
 									<div className="notify-bell pt-3 group cursor-pointer relative">
-
 										{messageCount > 0 && (
-											<div className="text-white text-[8px] absolute text-center px-1 bg-red-500 h-[15px] font-semibold top-3 right-2 rounded-full">{messageCount > 100 ? "99+" : messageCount}</div>
+											<div className="text-white text-[8px] absolute text-center px-1 bg-red-500 h-[15px] font-semibold top-3 right-2 rounded-full">
+												{messageCount > 100 ? "99+" : messageCount}
+											</div>
 										)}
 										<img className=" w-8 h-8 my-auto" src="/images/chat-icon.png" alt="" />
 									</div>
 								</Link>
-							}
-							<menu
-								className="menu btn d-flex d-md-none flex-column"
-								onClick={() => setMenu(!menu)}
-								id="button"
-								role="button"
-							>
+							)}
+							<menu className="menu btn d-flex d-md-none flex-column" onClick={() => setMenu(!menu)} id="button" role="button">
 								<div className="bars"></div>
 								<div className="bars"></div>
 								<div className="bars"></div>
@@ -151,35 +165,23 @@ const Header = (): JSX.Element => {
 								</a>
 							</Link>
 						</li> */}
-						{user ? (
-							navItems(Boolean(user)).map((nav, i) => (
-								<li onClick={() => localStorage.setItem("page", `${user?.id}`)} className="nav-item pt-2" key={i}>
-									<Link href={`${nav.link}`}>
-										<a
-											className={
-												pathname == `/${nav.link}`
-													? `border-b border-warning nav-link`
-													: ` nav-link`
-											}
-										>
-											{nav.title}
-										</a>
-									</Link>
-								</li>
-							))
-						) : null}
+						{user
+							? navItems(Boolean(user)).map((nav, i) => (
+									<li onClick={() => localStorage.setItem("page", `${user?.id}`)} className="nav-item pt-2" key={i}>
+										<Link href={`${nav.link}`}>
+											<a className={pathname == `/${nav.link}` ? `border-b border-warning nav-link` : ` nav-link`}>{nav.title}</a>
+										</Link>
+									</li>
+							  ))
+							: null}
 
 						<li className="my-auto nav-item nav-link">
 							<Dropdown title="Explore">
 								<Dropdown.Item>
-									<Link href="/campaigns">
-										Petitions
-									</Link>
+									<Link href="/campaigns">Petitions</Link>
 								</Dropdown.Item>
 								<Dropdown.Item>
-									<Link href="/events">
-										Events
-									</Link>
+									<Link href="/events">Events</Link>
 								</Dropdown.Item>
 							</Dropdown>
 						</li>
@@ -191,21 +193,17 @@ const Header = (): JSX.Element => {
 						<li className="nav-item">
 							{!user ? (
 								<Link href="/">
-									<button className="p-2 font-black px-5 mt-2 join rounded-pill bg-warning text-white">
-										Join
-									</button>
+									<button className="p-2 font-black px-5 mt-2 join rounded-pill bg-warning text-white">Join</button>
 								</Link>
 							) : (
-
-								<div className='flex'>
+								<div className="flex">
 									<Link href="/messages">
-										<div
-											className='notify-bell pt-3 group cursor-pointer relative'
-
-										>
+										<div className="notify-bell pt-3 group cursor-pointer relative">
 											{/* <img src="/images/ci_notification-outline-dot.svg" alt="" /> */}
 											{messageCount > 0 && (
-												<div className="text-white text-[8px] absolute text-center px-1 bg-red-500 h-[15px] font-semibold top-3 right-2 rounded-full">{messageCount > 100 ? "99+" : messageCount}</div>
+												<div className="text-white text-[8px] absolute text-center px-1 bg-red-500 h-[15px] font-semibold top-3 right-2 rounded-full">
+													{messageCount > 100 ? "99+" : messageCount}
+												</div>
 											)}
 											<svg xmlns="http://www.w3.org/2000/svg" width="19" height="19" fill="currentColor" className="bi bi-chat-dots-fill" viewBox="0 0 16 16">
 												<path d="M16 8c0 3.866-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.584.296-1.925.864-4.181 1.234-.2.032-.352-.176-.273-.362.354-.836.674-1.95.77-2.966C.744 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7zM5 8a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm4 0a1 1 0 1 0-2 0 1 1 0 0 0 2 0zm3 1a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" />
@@ -214,22 +212,16 @@ const Header = (): JSX.Element => {
 									</Link>
 
 									<Link href="/notifications">
-										<div
-											className='notify-bell pt-3 group cursor-pointer relative'
-
-										>
+										<div className="notify-bell pt-3 group cursor-pointer relative">
 											{/* <img src="/images/ci_notification-outline-dot.svg" alt="" /> */}
 											{count > 0 && (
-												<div className="text-white text-[8px] absolute text-center px-1 bg-red-500 h-[15px] font-semibold top-3 right-2 rounded-full">{count > 100 ? "99+" : count}</div>
+												<div className="text-white text-[8px] absolute text-center px-1 bg-red-500 h-[15px] font-semibold top-3 right-2 rounded-full">
+													{count > 100 ? "99+" : count}
+												</div>
 											)}
-											<svg
-												width="19"
-												height="25"
-												xmlns="http://www.w3.org/2000/svg"
-												viewBox="0 0 448 512"
-											>
-												<path d="M256 32V51.2C329 66.03 384 130.6 384 208V226.8C384 273.9 401.3 319.2 432.5 354.4L439.9 362.7C448.3 372.2 450.4 385.6 445.2 397.1C440 408.6 428.6 416 416 416H32C19.4 416 7.971 408.6 2.809 397.1C-2.353 385.6-.2883 372.2 8.084 362.7L15.5 354.4C46.74 319.2 64 273.9 64 226.8V208C64 130.6 118.1 66.03 192 51.2V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32H256zM224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512z" /></svg>
-
+											<svg width="19" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+												<path d="M256 32V51.2C329 66.03 384 130.6 384 208V226.8C384 273.9 401.3 319.2 432.5 354.4L439.9 362.7C448.3 372.2 450.4 385.6 445.2 397.1C440 408.6 428.6 416 416 416H32C19.4 416 7.971 408.6 2.809 397.1C-2.353 385.6-.2883 372.2 8.084 362.7L15.5 354.4C46.74 319.2 64 273.9 64 226.8V208C64 130.6 118.1 66.03 192 51.2V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32H256zM224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512z" />
+											</svg>
 										</div>
 									</Link>
 									{/* <div className='p-1'></div> */}
@@ -246,58 +238,38 @@ const Header = (): JSX.Element => {
 					{navItems(Boolean(user)).map((nav, i) => (
 						<div className="text-center py-2" key={i}>
 							<Link href={`${nav.link}`}>
-								<a
-									className={
-										pathname == `/${nav.link}`
-											? `text-warning fw-bold nav-link`
-											: ` nav-link fw-bold`
-									}
-								>
-									{nav.title}
-								</a>
+								<a className={pathname == `/${nav.link}` ? `text-warning fw-bold nav-link` : ` nav-link fw-bold`}>{nav.title}</a>
 							</Link>
 						</div>
 					))}
 					<div className="my-auto nav-item">
 						<Dropdown title="Explore">
 							<Dropdown.Item>
-								<Link href="/campaigns">
-									Petitions
-								</Link>
+								<Link href="/campaigns">Petitions</Link>
 							</Dropdown.Item>
 							<Dropdown.Item>
-								<Link href="/events">
-									Events
-								</Link>
+								<Link href="/events">Events</Link>
 							</Dropdown.Item>
 						</Dropdown>
 					</div>
 					{!user ? (
 						<Link href="/">
-							<button className="btn px-5 join rounded-pill bg-warning text-white font-weight-bold">
-								Join
-							</button>
+							<button className="btn px-5 join rounded-pill bg-warning text-white font-weight-bold">Join</button>
 						</Link>
 					) : (
 						<div className="flex justify-between">
 							<UserMenu />
 							<Link href="/notifications">
-								<div
-									className='notify-bell pt-3 group cursor-pointer relative'
-
-								>
+								<div className="notify-bell pt-3 group cursor-pointer relative">
 									{/* <img src="/images/ci_notification-outline-dot.svg" alt="" /> */}
 									{count > 0 && (
-										<div className="text-white text-[8px] absolute text-center px-1 bg-red-500 h-[15px] font-semibold top-3 right-2 rounded-full">{count > 100 ? "99+" : count}</div>
+										<div className="text-white text-[8px] absolute text-center px-1 bg-red-500 h-[15px] font-semibold top-3 right-2 rounded-full">
+											{count > 100 ? "99+" : count}
+										</div>
 									)}
-									<svg
-										width="19"
-										height="25"
-										xmlns="http://www.w3.org/2000/svg"
-										viewBox="0 0 448 512"
-									>
-										<path d="M256 32V51.2C329 66.03 384 130.6 384 208V226.8C384 273.9 401.3 319.2 432.5 354.4L439.9 362.7C448.3 372.2 450.4 385.6 445.2 397.1C440 408.6 428.6 416 416 416H32C19.4 416 7.971 408.6 2.809 397.1C-2.353 385.6-.2883 372.2 8.084 362.7L15.5 354.4C46.74 319.2 64 273.9 64 226.8V208C64 130.6 118.1 66.03 192 51.2V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32H256zM224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512z" /></svg>
-
+									<svg width="19" height="25" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+										<path d="M256 32V51.2C329 66.03 384 130.6 384 208V226.8C384 273.9 401.3 319.2 432.5 354.4L439.9 362.7C448.3 372.2 450.4 385.6 445.2 397.1C440 408.6 428.6 416 416 416H32C19.4 416 7.971 408.6 2.809 397.1C-2.353 385.6-.2883 372.2 8.084 362.7L15.5 354.4C46.74 319.2 64 273.9 64 226.8V208C64 130.6 118.1 66.03 192 51.2V32C192 14.33 206.3 0 224 0C241.7 0 256 14.33 256 32H256zM224 512C207 512 190.7 505.3 178.7 493.3C166.7 481.3 160 464.1 160 448H288C288 464.1 281.3 481.3 269.3 493.3C257.3 505.3 240.1 512 224 512z" />
+									</svg>
 								</div>
 							</Link>
 							{/* <Link href="/messages">
@@ -318,7 +290,7 @@ const Header = (): JSX.Element => {
 				</menu>
 			)}
 		</header>
-	);
-};
+	)
+}
 
-export default Header;
+export default Header
